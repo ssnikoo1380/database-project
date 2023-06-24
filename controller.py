aec34ef4d0b19b3a1c7721a426d79e1611e638fe
ssnikoo1380 <ssnikoo1380@gmail.com>
2023-06-25 01:40:42 +0330

    controller part for login section

---
+++ controller.py
@@ -0,0 +1,58 @@
+import model
+from model import user
+from model import message
+import view
+
+
+class Login:
+    usermodel = model.user()
+    messagemodel = model.message()
+
+    def __init__(self, show_in_login):
+        show_in_login.prompt("has account")
+        has_account = True if input().lower() == "y" else False
+        if has_account:
+            # login
+            while True:
+                show_in_login.prompt("get id")
+                login_id = input()
+                if not self.user_id_exists(login_id):
+                    show_in_login.prompt("no user")
+                    continue
+                break
+            while True:
+                show_in_login.prompt("get pass")
+                login_pass = input()
+                if not self.password_is_correct(login_id, login_pass):
+                    show_in_login.prompt("wrong pass")
+                    continue
+                break
+            show_in_login.prompt("login success")
+        else:
+            # sign up
+            while True:
+                show_in_login.prompt("get id")
+                signup_id = input()
+                if self.user_id_exists(signup_id):
+                    show_in_login.prompt("duplicate id")
+                    continue
+                show_in_login.prompt("get pass")
+                signup_pass = input()
+                show_in_login.prompt("set name")
+                signup_username = input()
+                break
+            show_in_login.prompt("signup success")
+
+    # checking if the entered username exists in database for login
+    def user_id_exists(self, user_id: str) -> bool:
+        return True if self.usermodel.return_user_id(user_id) else False
+
+    # same as username for password
+    def password_is_correct(self, user_id: str, user_pass: str) -> bool:
+        print(user_pass)
+        return True if self.usermodel.return_user_pass(user_id, user_pass) else False
+
+
+loginview = view.Login()
+
+Login(loginview)
