75ff0f0f0d23a0fba2d3a0db9cba03ee3c63c739
ssnikoo1380 <ssnikoo1380@gmail.com>
2023-06-28 22:16:38 +0330

    added add and delete chat finctionality

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 
-class user:
+class User:
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -26,10 +26,9 @@ class user:
         self.usercursor.execute(user_id_query, input_user_id)
         return self.usercursor.fetchall()
 
-    def return_user_pass(self, input_user_id, input_user_pass):
-        user_pass_query = """SELECT password FROM users WHERE user_id = %s and password = %s"""
-        input_user_pass = (input_user_id, input_user_pass, )
-        self.usercursor.execute(user_pass_query, input_user_pass)
+    def return_user_pass(self, input_user_id):
+        user_pass_query = """SELECT password FROM users WHERE user_id = %s"""
+        self.usercursor.execute(user_pass_query, (input_user_id, ))
         return self.usercursor.fetchall()
 
     def create_user(self, information):
@@ -37,26 +36,65 @@ class user:
             add_user_query = """INSERT INTO users (user_id, username, password) VALUES (%s, %s, %s)"""
             add_user_values = tuple(x for x in information)
             self.usercursor.execute(add_user_query, add_user_values)
-            mydb.commit()
+            self.mydb.commit()
         except Exception:
             return False
         return True
 
 
-class message:
-    def create_table(cursor):
-        cursor.execute("""CREATE TABLE messages(
+class Message:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="26642664",
+        database="chat_app"
+    )
+    messagecursor = mydb.cursor()
+
+    def send_message(self, sender, receiver, message):
+        try:
+            msgcrt_query = "INSERT INTO messages (text, receiver_id, is_read, FKuser_id) VALUES (%s, %s, %s, %s)"
+            msgcrt_values = (message, receiver, False, sender)
+            self.messagecursor.execute(msgcrt_query, msgcrt_values)
+            self.mydb.commit()
+        except Exception:
+            return False
+        return True
+
+    def chat_exists(self, receiver, sender):
+        query = "SELECT * FROM messages WHERE receiver_id = %s and FKuser_id = %s"
+        values = (receiver, sender)
+        self.messagecursor.execute(query, values)
+        return True if self.messagecursor.fetchall() else False
+
+    def delete_chat(self, receiver, sender):
+        try:
+            query = "DELETE FROM messages WHERE receiver_id = %s and FKuser_id = %s"
+            values = (receiver, sender)
+            self.messagecursor.execute(query, values)
+            self.mydb.commit()
+        except Exception:
+            return False
+        return True
+
+    def return_chatlist(self, sender):
+        query = "SELECT receiver_id FROM messages WHERE FKuser_id = %s "
+        values = (sender, )
+        self.messagecursor.execute(query, values)
+        return self.messagecursor.fetchall()
+
+    def create_table(self):
+        self.messagecursor.execute("""CREATE TABLE messages(
                 msg_id INT AUTO_INCREMENT NOT NULL,
                 text TEXT,
-                sender_id INT NOT NULL,
-                receiver_id INT NOT NULL,
+                receiver_id VARCHAR(255) NOT NULL,
                 is_read BIT,
-                FKuser_id INT NOT NULL,
+                FKuser_id VARCHAR(255) NOT NULL,
                 PRIMARY KEY (msg_id),
                 FOREIGN KEY (FKuser_id) REFERENCES users(user_id)
 
                                                 )"""
-                       )
+                                   )
 
 
 def create_database(cursor):
