265306033312271993a0a608c0f13013d32f449d
ssnikoo1380 <ssnikoo1380@gmail.com>
2023-06-28 22:15:04 +0330

    addedd add chat and delete chat functionality

--- view.py
+++ view.py
@@ -24,18 +24,33 @@ class Login:
 
 
 class Chatlist:
+    chatlist = []
+
     def prompt(self, to_show: str):
-        print("Options: \n")
-        print("1.Add a New Chat")
-        print("2.Delete a Chat")
-        print("3.Logout")
+        if to_show == "":
+            print("Options: \n")
+            print("1.Add a New Chat")
+            print("2.Delete a Chat")
+            print("3.Show Current Chats")
+            print("4.Logout")
         if to_show == "new":
             print("Enter The Name(s) Of The Person(s) You Want To Message: ")
         elif to_show == "no user":
             print("User Not Found!")
+        elif to_show == "ask make new":
+            print("1.Make A New Chat?")
+        elif to_show == "ask menu":
+            print("2.Return To Menu")
+        elif to_show == "not sent":
+            print("Unable To Send Message!")
         elif to_show == "delete":
             print("Enter The Name(s) Of The Chat(s) You Want To Delete: ")
         elif to_show == "delete success":
             print("Selected Chat(s) Deleted Successfully!")
+        elif to_show == "delete fail":
+            print("Unable To Delete!")
+        elif to_show == "current":
+            for chat in self.chatlist:
+                print(chat, "\n")
         elif to_show == "invalid option":
             print("Please Choose a Valid Option")
